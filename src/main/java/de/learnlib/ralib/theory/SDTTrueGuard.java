0edc0aae6a0b95363d8a850a23d6f5f2a74e1917
sacssel <sofiacassel@gmail.com>
2015-03-05 14:37:23 +0100

    true guard

---
+++ src/main/java/de/learnlib/ralib/theory/SDTTrueGuard.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.learnlib.ralib.theory;
+
+import de.learnlib.ralib.automata.guards.DataExpression;
+import de.learnlib.ralib.automata.guards.IfGuard;
+import de.learnlib.ralib.data.SymbolicDataValue;
+import gov.nasa.jpf.constraints.api.Expression;
+import gov.nasa.jpf.constraints.api.Variable;
+import gov.nasa.jpf.constraints.util.ExpressionUtil;
+import java.util.Map;
+
+/**
+ *
+ * @author Stealth
+ */
+public class SDTTrueGuard extends SDTGuard {
+
+    public SDTTrueGuard(SymbolicDataValue.SuffixValue param) {
+        super(param);
+    }
+
+    @Override
+    public String toString() {
+        return "TRUE";
+    }
+
+    @Override
+    public Expression<Boolean> toExpr() {
+        return ExpressionUtil.TRUE;
+    }
+
+    @Override
+    public IfGuard toTG(Map<SymbolicDataValue, Variable> variables) {
+        Expression<Boolean> expr = this.toExpr();
+        DataExpression<Boolean> cond = new DataExpression<>(expr, variables);
+        return new IfGuard(cond);
+
+    }
+
+}
