68f0d74dd73ba99d8c12937282046810bd97b3a9
Falk Howar <falk.howar@tu-clausthal.de>
2015-03-06 13:18:58 +0100

    porting random walk

--- src/main/java/de/learnlib/ralib/equivalence/IORandomWalk.java
+++ src/main/java/de/learnlib/ralib/equivalence/IORandomWalk.java
@@ -1,9 +1,40 @@
+/*
+ * Copyright (C) 2014 falk.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
 package de.learnlib.ralib.equivalence;
 
 
+import de.learnlib.ralib.automata.xml.RegisterAutomaton;
+import de.learnlib.ralib.data.Constants;
+import de.learnlib.ralib.data.DataType;
+import de.learnlib.ralib.data.DataValue;
+import de.learnlib.ralib.sul.DataWordSUL;
+import de.learnlib.ralib.theory.Theory;
+import de.learnlib.ralib.words.DataWords;
+import de.learnlib.ralib.words.PSymbolInstance;
+import de.learnlib.ralib.words.ParameterizedSymbol;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+import net.automatalib.words.Word;
 
 /**
  *
@@ -11,60 +42,58 @@ import java.util.Random;
  */
 public class IORandomWalk {
 
-    //TODO: port equivalence test
+
     
-//    private final Random rand;
-//    private IORASimulatorOracle hyp;
-//    private final IORASimulatorOracle target;
-//    private final ParameterizedSymbol[] inputs;
-//    private final boolean uniform;
-//    private final double resetProbability;
-//    private final long maxRuns;
-//    private final boolean resetRuns;
-//    private long runs;
-//    
-//    private final int maxDepth;
-//    
-//    private final Constants constants;
-//
-//    public IORandomWalk(Random rand, 
-//            IORASimulatorOracle target, Alphabet inputs,
-//            boolean uniform, double resetProbability, long maxRuns, int maxDepth,
-//            Constants constants, boolean resetRuns) {
-//        this.resetRuns = resetRuns;
-//        this.rand = rand;
-//        this.target = target;
-//        this.inputs = new ParameterizedSymbol[inputs.size()];
-//        int idx = 0;
-//        for (Symbol s : inputs.getSymbolList()) {
-//            ParameterizedSymbol ps = (ParameterizedSymbol) s;
-//            this.inputs[idx++] = ps;
-//        }
-//
-//        this.uniform = uniform;
-//        this.resetProbability = resetProbability;
-//        this.maxRuns = maxRuns;
-//        this.constants = constants;
-//        this.maxDepth = maxDepth;
-//    }
-//
-//    public Word findCounterExample(IORAAutomaton hyp) {
-//        updateHyp(hyp);
-//        if (resetRuns) {
-//            runs = 0;
-//        }
-//
-//        while (runs < maxRuns) {
-//            Word ce = run();
-//            if (ce != null) {
-//                return ce;
-//            }
-//        }
-//
-//        return null;
-//    }
-//
-//    private Word run() {        
+    private final Random rand;
+    private RegisterAutomaton hyp;
+    private final DataWordSUL target;
+    private final ParameterizedSymbol[] inputs;
+    private final boolean uniform;
+    private final double resetProbability;
+    private final double newDataProbability;
+    private final long maxRuns;
+    private final boolean resetRuns;
+    private long runs;
+    private final int maxDepth;
+    private final Constants constants;    
+    private final Map<DataType, Theory> teachers;
+
+    public IORandomWalk(Random rand, DataWordSUL target, boolean uniform, 
+            double resetProbability, double newDataProbability, long maxRuns, int maxDepth, Constants constants, 
+            boolean resetRuns, Map<DataType, Theory> teachers, ParameterizedSymbol ... inputs) {
+        
+        this.resetRuns = resetRuns;
+        this.rand = rand;
+        this.target = target;
+        this.inputs = inputs;
+        this.uniform = uniform;
+        this.resetProbability = resetProbability;
+        this.maxRuns = maxRuns;
+        this.constants = constants;
+        this.maxDepth = maxDepth;
+        this.teachers = teachers;
+        this.newDataProbability = newDataProbability;
+    }
+
+    public Word<PSymbolInstance> findCounterExample(RegisterAutomaton hyp) {
+        this.hyp = hyp;        
+        // reset the counter for number of runs?
+        if (resetRuns) {
+            runs = 0;
+        }
+        // find counterexample ...
+        while (runs < maxRuns) {
+            Word ce = run();
+            if (ce != null) {
+                return ce;
+            }
+        }
+        return null;
+    }
+    
+    
+
+    private Word run() {        
 //        int depth = 0;
 //        runs++;
 //        List<DataValue> usedValsSys = new ArrayList<DataValue>();
@@ -114,72 +143,81 @@ public class IORandomWalk {
 //        
 //        //System.out.println("RUN: " + testSys);
 //        return null;
-//    }
-//
-//    private PSymbolInstance[] pickInput(
-//            List<DataValue> usedValsSys, List<DataValue> usedValsHyp) {
-//        // pick symbol
-//        ParameterizedSymbol ps = null;
-//        if (uniform) {
-//            ps = inputs[rand.nextInt(inputs.length)];
-//        } else {
-//            int MAX_WEIGHT = 0;
-//            int[] weights = new int[inputs.length];
-//            for (int i = 0; i < weights.length; i++) {
-//                weights[i] = 1;
-//                for (int j = 0; j < inputs[i].getArity(); j++) {
-//                    weights[i] *= (usedValsSys.size() + j + 1);
-//                }
-//                MAX_WEIGHT += weights[i];
-//            }
-//
-//            int idx = rand.nextInt(MAX_WEIGHT) + 1;
-//            int sum = 0;
-//            for (int i = 0; i < inputs.length; i++) {
-//                sum += weights[i];
-//                if (idx <= sum) {
-//                    ps = inputs[i];
-//                    break;
-//                }
-//            }
-//        }
-//        // pick data values
-//        assert ps != null;
-//        Object[] dataSys = new Object[ps.getArity()];
-//        Object[] dataHyp = new Object[ps.getArity()];
-//        for (int i = 0; i < ps.getArity(); i++) {
-//            int idx = rand.nextInt(Math.max(usedValsSys.size() * 2, 1));
-//            idx = Math.min(idx, usedValsSys.size());
-//            DataValue dSys;
-//            DataValue dHyp;
-//            if (idx >= usedValsSys.size()) {
-//                dSys = new DataValue(idx + 1);
-//                dHyp = new DataValue(idx + 1);
-//                usedValsSys.add(dSys);
-//                usedValsHyp.add(dHyp);
-//            } else {
-//                dSys = usedValsSys.get(idx);
-//                dHyp = usedValsHyp.get(idx);
-//            }
-//
-//            dataSys[i] = dSys;
-//            dataHyp[i] = dHyp; 
-//        }
-//
-//        return new PSymbolInstance[] { 
-//            new PSymbolInstance(ps, dataSys),
-//            new PSymbolInstance(ps, dataHyp) };
-//    }
-//
-//    
-//    private void updateHyp(IORAAutomaton hyp) {        
-//        this.hyp = new IORASimulatorOracle(hyp, new ValueGenerator() {
-//            int vv = 2000000;
-//            @Override
-//            public DataValue generateValue(Register r, DataValue... blocked) {
-//                return new DataValue(vv++);
-//            }
-//        });
-//    }      
+        throw new UnsupportedOperationException();
+    }
+    
+    private PSymbolInstance nextInput(Word<PSymbolInstance> run) {
+        ParameterizedSymbol ps = nextSymbol(run);
+        PSymbolInstance psi = nextDataValues(run, ps);
+        return psi;
+    }
+
+    private PSymbolInstance nextDataValues(
+            Word<PSymbolInstance> run, ParameterizedSymbol ps) {
+        
+        DataValue[] vals = new DataValue[ps.getArity()];
+        
+        int i = 0;
+        for (DataType t : ps.getPtypes()) {
+            Theory teacher = teachers.get(t);
+            // TODO: generics hack?
+            // TODO: add constants?
+            Set<DataValue<Object>> oldSet = DataWords.valSet(run, t);
+            for (int j=0; j<i; j++) {
+                if (vals[j].getType().equals(t)) {
+                    oldSet.add(vals[j]);
+                }
+            }
+            ArrayList<DataValue<Object>> old = new ArrayList<>(oldSet);
+            
+            double draw = rand.nextDouble();
+            if (draw <= newDataProbability) {
+                DataValue v = teacher.getFreshValue(old);
+                vals[i] = v;
+            }
+            else {
+                int idx = rand.nextInt(old.size());  
+                vals[i] = old.get(idx);
+            }
+            
+            i++;
+        }
+        return new PSymbolInstance(ps, vals);
+    }
+    
+    private ParameterizedSymbol nextSymbol(Word<PSymbolInstance> run) {
+        ParameterizedSymbol ps = null;
+        Map<DataType, Integer> tCount = new HashMap<>();
+        if (uniform) {
+            ps = inputs[rand.nextInt(inputs.length)];
+        } else {
+            int MAX_WEIGHT = 0;
+            int[] weights = new int[inputs.length];
+            for (int i = 0; i < weights.length; i++) {
+                weights[i] = 1;
+                for (DataType t : inputs[i].getPtypes()) {
+                    Integer old = tCount.get(t);
+                    if (old == null) {
+                        // TODO: what about constants?
+                        old = 0;
+                    }                    
+                    weights[i] *= (old + 1);
+                    tCount.put(t, ++old);
+                }
+                MAX_WEIGHT += weights[i];
+            }
+
+            int idx = rand.nextInt(MAX_WEIGHT) + 1;
+            int sum = 0;
+            for (int i = 0; i < inputs.length; i++) {
+                sum += weights[i];
+                if (idx <= sum) {
+                    ps = inputs[i];
+                    break;
+                }
+            }
+        }
+        return ps;
+    }
 
 }
