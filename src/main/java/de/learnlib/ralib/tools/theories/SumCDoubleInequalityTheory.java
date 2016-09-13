8d822eb81c742f60f0fa03c86bd6d7a09bc6dcca
Paul Fiterau Brostean <Paul Fiterau Brostean>
2016-09-13 16:04:19 +0200

    Not yet working

---
+++ src/main/java/de/learnlib/ralib/tools/theories/SumCDoubleInequalityTheory.java
@@ -0,0 +1,49 @@
+package de.learnlib.ralib.tools.theories;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import de.learnlib.ralib.data.DataType;
+import de.learnlib.ralib.data.DataValue;
+
+public class SumCDoubleInequalityTheory extends DoubleInequalityTheory{
+	
+	private List<DataValue<Double>> sumConstants;
+	private List<DataValue<Double>> regularConstants;
+
+	public SumCDoubleInequalityTheory(DataType dataType) {
+		this(dataType, Collections.emptyList(), Collections.emptyList());
+	}
+	
+	public SumCDoubleInequalityTheory(DataType dataType, List<DataValue<Double>> sumConstants, List<DataValue<Double>> regularConstants) {
+		super(dataType);
+		this.sumConstants = sumConstants;
+		this.regularConstants = regularConstants;
+	}
+	
+    public List<DataValue<Double>> getPotential(List<DataValue<Double>> dvs) {
+        //assume we can just sort the list and get the values
+        List<DataValue<Double>> sortedList = makeNewPotsWithSumC(dvs);
+        
+        //sortedList.addAll(dvs);
+        Collections.sort(sortedList, new Cpr());
+
+        //System.out.println("I'm sorted!  " + sortedList.toString());
+        return sortedList;
+    }
+    
+    private List<DataValue<Double>> makeNewPotsWithSumC(List<DataValue<Double>> dvs) {
+    	List<DataValue<Double>> sortedList = dvs.stream().filter(potVal -> !regularConstants.contains(potVal)).  // we filter the actual constants (we don't want sums over constants)
+    			flatMap(value -> sumConstants.stream().map(c -> addNumbers(value, c))). // apply each of the sum constants to the potential
+    			distinct(). // remove any duplicates
+    			collect(Collectors.toList()); // collect them to a list
+    	        sortedList.addAll(dvs); // add the initial data values
+    	        return sortedList;
+    }
+
+	private DataValue<Double> addNumbers(DataValue<Double> value, DataValue<Double> c) {
+		return new DataValue<Double>(value.getType(), value.getId() + c.getId());
+	}
+
+}
---
+++ src/test/java/de/learnlib/ralib/theory/TestSumCIneqEqTree.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2014 falk.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package de.learnlib.ralib.theory;
+
+import de.learnlib.ralib.RaLibTestSuite;
+import de.learnlib.ralib.TestUtil;
+import de.learnlib.ralib.data.Constants;
+import de.learnlib.ralib.oracles.mto.MultiTheoryTreeOracle;
+import de.learnlib.ralib.oracles.TreeQueryResult;
+import de.learnlib.ralib.data.DataType;
+import de.learnlib.ralib.data.DataValue;
+import de.learnlib.ralib.data.PIV;
+import de.learnlib.ralib.data.SymbolicDataValue.Parameter;
+import de.learnlib.ralib.data.SymbolicDataValue.Register;
+import de.learnlib.ralib.learning.SymbolicDecisionTree;
+import de.learnlib.ralib.learning.SymbolicSuffix;
+import de.learnlib.ralib.oracles.Branching;
+import de.learnlib.ralib.example.priority.PriorityQueueSUL;
+import de.learnlib.ralib.solver.jconstraints.JConstraintsConstraintSolver;
+import de.learnlib.ralib.words.PSymbolInstance;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import net.automatalib.words.Word;
+import org.testng.annotations.Test;
+
+import de.learnlib.ralib.tools.theories.DoubleInequalityTheory;
+import de.learnlib.ralib.tools.theories.SumCDoubleInequalityTheory;
+
+import java.util.logging.Level;
+import org.testng.Assert;
+
+/**
+ *
+ * @author falk
+ */
+public class TestSumCIneqEqTree extends RaLibTestSuite {
+
+    @Test
+    public void testIneqEqTree() {
+
+        final Map<DataType, Theory> teachers = new LinkedHashMap<>();
+        teachers.put(PriorityQueueSUL.DOUBLE_TYPE, 
+                new SumCDoubleInequalityTheory(PriorityQueueSUL.DOUBLE_TYPE, 
+                		Arrays.asList(new DataValue<Double>(PriorityQueueSUL.DOUBLE_TYPE, 100.0)), 
+                        		Collections.emptyList()));
+
+        PriorityQueueSUL sul = new PriorityQueueSUL();
+        JConstraintsConstraintSolver jsolv = TestUtil.getZ3Solver();        
+        MultiTheoryTreeOracle mto = TestUtil.createMTO(
+                sul, PriorityQueueSUL.ERROR, teachers, 
+                new Constants(), jsolv, 
+                sul.getInputSymbols());
+                
+        final Word<PSymbolInstance> longsuffix = Word.fromSymbols(
+                new PSymbolInstance(PriorityQueueSUL.POLL),
+                new PSymbolInstance(PriorityQueueSUL.OUTPUT,
+                        new DataValue(PriorityQueueSUL.DOUBLE_TYPE, 4.0)),
+                new PSymbolInstance(PriorityQueueSUL.OFFER,
+                        new DataValue(PriorityQueueSUL.DOUBLE_TYPE, 5.0)),
+                new PSymbolInstance(PriorityQueueSUL.OK),
+                new PSymbolInstance(PriorityQueueSUL.POLL),
+                new PSymbolInstance(PriorityQueueSUL.OUTPUT,
+                        new DataValue(PriorityQueueSUL.DOUBLE_TYPE, 6.0)));
+        
+        final Word<PSymbolInstance> prefix = Word.fromSymbols(
+                new PSymbolInstance(PriorityQueueSUL.OFFER,
+                        new DataValue(PriorityQueueSUL.DOUBLE_TYPE, 1.0)),
+                new PSymbolInstance(PriorityQueueSUL.OK),
+                new PSymbolInstance(PriorityQueueSUL.OFFER,
+                        new DataValue(PriorityQueueSUL.DOUBLE_TYPE, 2.0)),
+                new PSymbolInstance(PriorityQueueSUL.OK),
+                new PSymbolInstance(PriorityQueueSUL.OFFER,
+                        new DataValue(PriorityQueueSUL.DOUBLE_TYPE, 3.0)),
+                new PSymbolInstance(PriorityQueueSUL.OK));
+
+        // create a symbolic suffix from the concrete suffix
+        // symbolic data values: s1, s2 (userType, passType)
+        final SymbolicSuffix symSuffix = new SymbolicSuffix(prefix, longsuffix);
+        logger.log(Level.FINE, "Prefix: {0}", prefix);
+        logger.log(Level.FINE, "Suffix: {0}", symSuffix);
+
+        TreeQueryResult res = mto.treeQuery(prefix, symSuffix);
+        SymbolicDecisionTree sdt = res.getSdt();
+
+        final String expectedTree = "[r2, r1]-+\n" +
+"        []-(s1!=r2)\n" +
+"         |    []-TRUE: s2\n" +
+"         |          []-TRUE: s3\n" +
+"         |                [Leaf-]\n" +
+"         +-(s1=r2)\n" +
+"              []-(s2=r1)\n" +
+"               |    []-(s3!=s2)\n" +
+"               |     |    [Leaf-]\n" +
+"               |     +-(s3=s2)\n" +
+"               |          [Leaf+]\n" +
+"               +-(s2<r1)\n" +
+"               |    []-(s3!=s2)\n" +
+"               |     |    [Leaf-]\n" +
+"               |     +-(s3=s2)\n" +
+"               |          [Leaf+]\n" +
+"               +-(s2>r1)\n" +
+"                    []-(s3!=r1)\n" +
+"                     |    [Leaf-]\n" +
+"                     +-(s3=r1)\n" +
+"                          [Leaf+]\n";
+        
+        String tree = sdt.toString();
+        Assert.assertEquals(tree, expectedTree);
+        logger.log(Level.FINE, "final SDT: \n{0}", tree);
+
+        Parameter p1 = new Parameter(PriorityQueueSUL.DOUBLE_TYPE, 1);
+        Parameter p2 = new Parameter(PriorityQueueSUL.DOUBLE_TYPE, 2);
+        Parameter p3 = new Parameter(PriorityQueueSUL.DOUBLE_TYPE, 3);
+
+        PIV testPiv = new PIV();
+        testPiv.put(p1, new Register(PriorityQueueSUL.DOUBLE_TYPE, 1));
+        testPiv.put(p2, new Register(PriorityQueueSUL.DOUBLE_TYPE, 2));
+        testPiv.put(p3, new Register(PriorityQueueSUL.DOUBLE_TYPE, 3));
+
+        Branching b = mto.getInitialBranching(
+                prefix, PriorityQueueSUL.OFFER, testPiv, sdt);
+
+        Assert.assertEquals(b.getBranches().size(), 2);
+        logger.log(Level.FINE, "initial branching: \n{0}", b.getBranches().toString());
+    }
+
+
+}
