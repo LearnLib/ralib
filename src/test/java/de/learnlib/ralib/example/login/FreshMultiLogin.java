bf2681b19e551adf6ac7fbfcfa29b9ab6bacd0a2
Falk Howar <falk.howar@tu-clausthal.de>
2016-09-21 15:30:02 +0200

    fixed unstable implementation of fresh values in class-analyzer

--- src/test/java/de/learnlib/ralib/example/login/FreshMultiLogin.java
+++ src/test/java/de/learnlib/ralib/example/login/FreshMultiLogin.java
@@ -21,19 +21,16 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
 
-
 public class FreshMultiLogin {
 
     // Initialize statemachine constants,variables and locations
-    
-    HashMap < Integer,Integer > id2pwd = new HashMap < Integer,Integer > ();
-    HashMap < Integer,Boolean > id2loggedin = new HashMap < Integer,Boolean > ();
+    HashMap< Integer, Integer> id2pwd = new HashMap<>();
+    HashMap< Integer, Boolean> id2loggedin = new HashMap<>();
 
-	private int MAX_REGISTERED_USERS=2;
-	private int MAX_LOGGEDIN_USERS=1000000;
-	private int loggedin_users=0;
-	private Random random = new Random();
-
+    private final int MAX_REGISTERED_USERS = 3;
+    private final int MAX_LOGGEDIN_USERS = 1000000;
+    private int loggedin_users = 0;
+    private final Random random = new Random();
 
     //handling each Input
 
@@ -45,13 +42,13 @@ public class FreshMultiLogin {
      */
     public Integer IRegister(Integer uid) {
         Integer pwd = random.nextInt(10000000);
-        if ( ! id2pwd.containsKey(uid)  && id2pwd.keySet().size() < MAX_REGISTERED_USERS ) {
-        	id2pwd.put(uid, pwd);
-        	id2loggedin.put(uid, false);
-        } 
+        if (!id2pwd.containsKey(uid) && id2pwd.keySet().size() < MAX_REGISTERED_USERS) {
+            id2pwd.put(uid, pwd);
+            id2loggedin.put(uid, false);
+        }
         return pwd;
-    }    
-    
+    }
+
     /* login an user with uid
      * 
      * notes:
@@ -59,51 +56,49 @@ public class FreshMultiLogin {
      *       + is registered 
      *       + and is not logged in
      *   - at max only MAX_LOGGEDIN_USERS users may be logged in 
-     */   
-    public boolean ILogin(Integer uid,Integer pwd) {
-        if ( id2pwd.containsKey(uid) 
-        		&& ! id2loggedin.get(uid) 
-        		&& pwd == id2pwd.get(uid)
-        		&& loggedin_users < MAX_LOGGEDIN_USERS 
-            ) {
+     */
+    public boolean ILogin(Integer uid, Integer pwd) {
+        if (id2pwd.containsKey(uid)
+                && !id2loggedin.get(uid)
+                && pwd == id2pwd.get(uid)
+                && loggedin_users < MAX_LOGGEDIN_USERS) {
             loggedin_users++;
-        	id2loggedin.put(uid, true );
-        	return true;
-        } 
+            id2loggedin.put(uid, true);
+            return true;
+        }
         return false;
     }
-    
+
     /* ILogout
      * 
      * A user can only logout when logged in.
      */
     public boolean ILogout(Integer uid) {
-        if ( id2loggedin.containsKey(uid) && id2loggedin.get(uid) ) {
-        	id2loggedin.put(uid, false );
+        if (id2loggedin.containsKey(uid) && id2loggedin.get(uid)) {
+            id2loggedin.put(uid, false);
             loggedin_users--;
             return true;
-        } 
+        }
         return false;
     }
-    
- 
+
     /* IChangePassword
      * 
      * a  user can only change password when logged in
      */
     public boolean IChangePassword(Integer uid, Integer pwd) {
 
-        if (  id2loggedin.containsKey(uid) &&  id2loggedin.get(uid) ) {
-        	id2pwd.put(uid, pwd);
-            return true;        	
-        } 
+        if (id2loggedin.containsKey(uid) && id2loggedin.get(uid)) {
+            id2pwd.put(uid, pwd);
+            return true;
+        }
         return false;
-    }      
+    }
 
-    public static void main(String args []) {
-    	Class<?> cls = FreshMultiLogin.class;
-    	for ( Method meth : cls.getMethods()) {
-    		System.out.println(Arrays.asList(meth.getParameterTypes()));
-    	}
+    public static void main(String args[]) {
+        Class<?> cls = FreshMultiLogin.class;
+        for (Method meth : cls.getMethods()) {
+            System.out.println(Arrays.asList(meth.getParameterTypes()));
+        }
     }
 }
