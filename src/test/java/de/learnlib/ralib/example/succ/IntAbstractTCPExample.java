45555f4c786d9306194aa5f574935346cab69404
Paul Fiterau Brostean <Paul Fiterau Brostean>
2017-03-02 20:43:35 +0100

    Test SULs in synch

---
+++ src/test/java/de/learnlib/ralib/example/succ/IntAbstractTCPExample.java
@@ -0,0 +1,53 @@
+package de.learnlib.ralib.example.succ;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class IntAbstractTCPExample extends IntAbstractWindowProtocol{
+	protected State state;
+	protected Set<Option> options;
+	
+	public IntAbstractTCPExample(Integer win) {
+		super(win);
+		configure(Option.WIN_SYNRECEIVED_TO_CLOSED, Option.WIN_SYNSENT_TO_CLOSED);
+		state = State.CLOSED;
+	}
+	
+	public IntAbstractTCPExample() {
+		super();
+		configure(Option.WIN_SYNRECEIVED_TO_CLOSED, Option.WIN_SYNSENT_TO_CLOSED);
+		state = State.CLOSED;
+	}
+	
+	public void configure (Option ... options) {
+		this.options = Arrays.asList(options).stream().collect(Collectors.toSet());
+	}
+	
+	public static enum Option {
+		/**
+		 * Activates transition from SYN_RECEIVED to CLOSED on an out of window client seq
+		 */
+		WIN_SYNRECEIVED_TO_CLOSED,
+		/**
+		 * Activates transition from SYN_SENT to CLOSED on an out of window server seq
+		 */
+		WIN_SYNSENT_TO_CLOSED;
+	}
+	
+	public enum State{
+		//	INIT,
+		// connect...
+		CONNECTING, // special state after a connect call has been received, before issuing of a SYN
+		// s(10,0)
+		SYN_SENT, 
+		// sa(20,11)
+		SYN_RECEIVED, // special state after a SYN+ACK has been received, before issuing of an ACK  
+		// a(11, 21)
+		ESTABLISHED,
+		FIN_WAIT_1,
+		TIME_WAIT,		
+		CLOSEWAIT,
+		CLOSED;
+	}
+}
---
+++ src/test/java/de/learnlib/ralib/example/succ/IntModerateFreshTCPExample.java
@@ -0,0 +1,109 @@
+package de.learnlib.ralib.example.succ;
+
+public class IntModerateFreshTCPExample extends IntAbstractTCPExample{
+
+	private Integer clSeq = null;
+	private Integer svSeq = null;
+	private State state = State.CLOSED;
+	// if true, the looping variable makes it always possible to transition out of the CLOSED state by a Connect. 
+	private boolean looping = true;
+	
+	public IntModerateFreshTCPExample(Integer window) {
+		super(window);
+	}
+	
+	public IntModerateFreshTCPExample() {
+		super();
+	}
+
+    public Integer IConnect() {
+    	Integer ret = super.newFresh();
+    	if (state == State.CLOSED && 
+    			(looping || this.clSeq == null) 
+    			) {
+    		this.clSeq = ret;
+    		state = State.CONNECTING;
+    	}
+        return ret;
+    }     
+    
+    public boolean ISYN(Integer seq, Integer ack) {
+    	boolean ret = false;
+    	if (state == State.CONNECTING) {
+    		if (seq.equals(clSeq)) {
+    			ret = true;
+    			state = State.SYN_SENT;
+    		}
+    	}
+    	
+    	return ret;
+    }
+    
+    public boolean ISYNACK(Integer seq, Integer ack) {
+    	boolean ret = false;
+    	if (state == State.SYN_SENT) {
+    		if (succ(clSeq, ack)) {
+    			ret = true;
+    			clSeq = ack;
+    			svSeq = seq;
+    			state = State.SYN_RECEIVED;
+    		} else {
+    			if(!inWin(this.clSeq, ack) && options.contains(Option.WIN_SYNSENT_TO_CLOSED)) 
+    				state = State.CLOSED;
+    			
+    		}
+    	}
+    	return ret;
+    }
+    
+    public boolean IACK(Integer seq, Integer ack) {
+    	boolean ret = false;
+    	if (state == State.SYN_RECEIVED) {
+    		if (equ(seq, clSeq) && succ(svSeq, ack)) {
+    			ret = true;
+    			svSeq = ack;
+    			state = State.ESTABLISHED;
+    		} 
+    	}
+    	
+    	if (state == State.ESTABLISHED) {
+    		if (equ(seq, clSeq) && succ(svSeq, ack) ||  
+    				equ(seq, clSeq) && equ(svSeq, ack)) {
+    			clSeq = seq;
+    			svSeq = ack;
+    			
+    			ret = true;
+    		} 
+    	}
+    	
+    	return ret;
+    }
+    
+
+    public boolean IFINACK(Integer seq, Integer ack) {
+    	boolean ret = false;
+    	if (state == State.ESTABLISHED) {
+    		if (seq.equals(clSeq) && succ(svSeq, ack) ||  
+    				seq.equals(clSeq) && ack.equals(svSeq)) {
+    			state = State.CLOSEWAIT;
+    			
+    			ret = true;
+    		} else if (
+    				seq.equals(svSeq) && succ(clSeq, ack) ||
+    				seq.equals(svSeq) && ack.equals(clSeq)) {
+    				state = State.CLOSEWAIT;
+    	    		ret = true;
+    		} 
+    	}
+    	
+    	return ret;
+    }
+    
+    public boolean ICLOSE() {
+    	if (state == State.CLOSEWAIT) {
+    		state = State.CLOSED;
+    		return true;
+    	}
+    	return false;
+    }
+}
