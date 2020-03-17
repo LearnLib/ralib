863afe1c511a59334433638ef1fe46d456024185
Paul <p.fiterau-brostean@science.ru.nl>
2020-03-17 14:13:07 +0100

    fewer tests

--- src/test/java/de/learnlib/ralib/learning/LearnBharatExampleTest.java
+++ src/test/java/de/learnlib/ralib/learning/LearnBharatExampleTest.java
@@ -153,7 +153,7 @@ public class LearnBharatExampleTest {
                 false, // do not draw symbols uniformly 
                 0.1, // reset probability 
                 0.8, // prob. of choosing a fresh data value
-                10000, // 1000 runs 
+                1000, // 1000 runs 
                 100, // max depth
                 consts,
                 false, // reset runs 
