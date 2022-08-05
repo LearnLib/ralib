73a82adb14061c004a2c2acec77d20157cde1fd5
Paul Fiterau <fiteraup@yahoo.com>
2022-08-05 11:09:44 +0200

    Added learning algorithm abstraction.

---
+++ src/main/java/de/learnlib/ralib/learning/RaLearningAlgorithm.java
@@ -0,0 +1,30 @@
+package de.learnlib.ralib.learning;
+
+import de.learnlib.oracles.DefaultQuery;
+import de.learnlib.ralib.words.PSymbolInstance;
+
+public interface RaLearningAlgorithm {
+
+	/**
+	 * Creates a hypothesis consistent with the counterexamples added thus far.
+	 */
+	public void learn();
+
+	/**
+	 * Provides a counterexample to the learner. Should be followed by {@link RaLearningAlgorithm#learn()}, which
+	 * will prompt the learner to refine the hypothesis based on the supplied
+	 * counterexample.
+	 * 
+	 * @param ce the query which exposes diverging behavior, as posed to the real
+	 *           SUL (i.e. with the SULs output).
+	 *
+	 */
+	public void addCounterexample(DefaultQuery<PSymbolInstance, Boolean> ce);
+
+	/**
+	 * Returns the last created hypothesis.
+	 */
+	public Hypothesis getHypothesis();
+	
+	public RaLearningAlgorithmName getName();
+}
