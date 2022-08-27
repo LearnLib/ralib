53f8332f5a50fc2bd001110582452bce215a5a1c
Fredrik Tåquist <fredriktaquist@gmail.com>
2022-08-27 15:12:36 +0200

    quantitative measurements during learning

---
+++ src/main/java/de/learnlib/ralib/learning/Measurements.java
@@ -0,0 +1,38 @@
+package de.learnlib.ralib.learning;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+
+import de.learnlib.ralib.words.PSymbolInstance;
+import net.automatalib.words.Word;
+
+public class Measurements {
+	public int treeQueries = 0;
+	public long memQueries = 0;
+	public final Map<SymbolicWord, Integer> treeQueryWords = new LinkedHashMap<SymbolicWord, Integer>();
+	public final Collection<Word<PSymbolInstance>> ces = new LinkedHashSet<Word<PSymbolInstance>>();
+	
+	public Measurements() {
+	}
+	
+	public Measurements(Measurements m) {
+		treeQueries = m.treeQueries;
+		memQueries = m.memQueries;
+		treeQueryWords.putAll(m.treeQueryWords);
+		ces.addAll(m.ces);
+	}
+	
+	public void reset() {
+		treeQueries = 0;
+		memQueries = 0;
+		treeQueryWords.clear();
+		ces.clear();
+	}
+	
+	@Override
+	public String toString() {
+		return "{TQ: " + treeQueries + ", MQ: " + memQueries + "}";
+	}
+}
