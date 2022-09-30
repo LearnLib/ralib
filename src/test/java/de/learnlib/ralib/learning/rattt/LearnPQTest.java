31bb922ceb67ae7896531ed2229823cbd9a23d92
Paul Fiterau <fiteraup@yahoo.com>
2022-09-30 12:08:31 +0200

    Code for computing random measurements.

--- src/test/java/de/learnlib/ralib/learning/rattt/LearnPQTest.java
+++ src/test/java/de/learnlib/ralib/learning/rattt/LearnPQTest.java
@@ -18,6 +18,18 @@
  */
 package de.learnlib.ralib.learning.rattt;
 
+import static de.learnlib.ralib.example.priority.PriorityQueueOracle.OFFER;
+import static de.learnlib.ralib.example.priority.PriorityQueueOracle.POLL;
+import static de.learnlib.ralib.example.priority.PriorityQueueOracle.doubleType;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.logging.Level;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import de.learnlib.oracles.DefaultQuery;
 import de.learnlib.ralib.RaLibLearningExperimentRunner;
 import de.learnlib.ralib.RaLibTestSuite;
@@ -26,29 +38,20 @@ import de.learnlib.ralib.automata.RegisterAutomaton;
 import de.learnlib.ralib.data.Constants;
 import de.learnlib.ralib.data.DataType;
 import de.learnlib.ralib.data.DataValue;
-import de.learnlib.ralib.learning.Hypothesis;
+import de.learnlib.ralib.learning.Measurements;
 import de.learnlib.ralib.learning.RaLearningAlgorithmName;
-
-import static de.learnlib.ralib.example.priority.PriorityQueueOracle.*;
 import de.learnlib.ralib.oracles.DataWordOracle;
 import de.learnlib.ralib.oracles.SDTLogicOracle;
 import de.learnlib.ralib.oracles.SimulatorOracle;
 import de.learnlib.ralib.oracles.TreeOracleFactory;
 import de.learnlib.ralib.oracles.mto.MultiTheorySDTLogicOracle;
 import de.learnlib.ralib.oracles.mto.MultiTheoryTreeOracle;
-import de.learnlib.ralib.solver.ConstraintSolver;
 import de.learnlib.ralib.solver.jconstraints.JConstraintsConstraintSolver;
 import de.learnlib.ralib.theory.Theory;
 import de.learnlib.ralib.tools.theories.DoubleInequalityTheory;
 import de.learnlib.ralib.words.PSymbolInstance;
 import de.learnlib.ralib.words.ParameterizedSymbol;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.logging.Level;
 import net.automatalib.words.Word;
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
 /**
  *
@@ -77,30 +80,31 @@ public class LearnPQTest extends RaLibTestSuite {
                 new MultiTheoryTreeOracle(new SimulatorOracle(hyp), teachers, new Constants(), jsolv);
 
         RaTTT rastar = new RaTTT(mto, hypFactory, mlo, consts, OFFER, POLL);
+        rastar.setUseOldAnalyzer(true);
         rastar.learn();
         rastar.doThoroughCESearch(true);
         RegisterAutomaton hyp = rastar.getHypothesis();
         logger.log(Level.FINE, "HYP1: {0}", hyp);
 
-        Word<PSymbolInstance> ce = Word.fromSymbols(
-                new PSymbolInstance(OFFER,
-                        new DataValue(doubleType, 1.0)),
-                new PSymbolInstance(OFFER,
-                        new DataValue(doubleType, 1.0)),
-                new PSymbolInstance(POLL,
-                        new DataValue(doubleType, 1.0)),
-                new PSymbolInstance(POLL,
-                        new DataValue(doubleType, 1.0)));
-
-        DefaultQuery<PSymbolInstance, Boolean> ceQuery = new DefaultQuery<>(ce, true);
-
-        rastar.addCounterexample(ceQuery);
-
-        rastar.learn();
-        hyp = rastar.getHypothesis();
-        logger.log(Level.FINE, "HYP2: {0}", hyp);
-        
-        Assert.assertTrue(hyp.accepts(ceQuery.getInput()));
+//        Word<PSymbolInstance> ce = Word.fromSymbols(
+//                new PSymbolInstance(OFFER,
+//                        new DataValue(doubleType, 1.0)),
+//                new PSymbolInstance(OFFER,
+//                        new DataValue(doubleType, 1.0)),
+//                new PSymbolInstance(POLL,
+//                        new DataValue(doubleType, 1.0)),
+//                new PSymbolInstance(POLL,
+//                        new DataValue(doubleType, 1.0)));
+//
+//        DefaultQuery<PSymbolInstance, Boolean> ceQuery = new DefaultQuery<>(ce, true);
+//
+//        rastar.addCounterexample(ceQuery);
+//
+//        rastar.learn();
+//        hyp = rastar.getHypothesis();
+//        logger.log(Level.FINE, "HYP2: {0}", hyp);
+//        
+//        Assert.assertTrue(hyp.accepts(ceQuery.getInput()));
         
         Word<PSymbolInstance> ce2 = Word.fromSymbols(
                 new PSymbolInstance(OFFER,
@@ -122,7 +126,7 @@ public class LearnPQTest extends RaLibTestSuite {
     
     @Test
     public void learnPQRandom() {
-        int SEEDS = 10;
+        int SEEDS = 1;
         Constants consts = new Constants();
         DataWordOracle dwOracle = 
                 new de.learnlib.ralib.example.priority.PriorityQueueOracle(2);
@@ -132,22 +136,24 @@ public class LearnPQTest extends RaLibTestSuite {
 
         
         JConstraintsConstraintSolver jsolv = TestUtil.getZ3Solver();       
-        MultiTheoryTreeOracle mto = new MultiTheoryTreeOracle(
-                dwOracle, teachers, new Constants(), jsolv);
-        
-        SDTLogicOracle mlo = new MultiTheorySDTLogicOracle(consts, jsolv);
-
-        TreeOracleFactory hypFactory = (RegisterAutomaton hyp) -> 
-                new MultiTheoryTreeOracle(new SimulatorOracle(hyp), teachers, new Constants(), jsolv);
         RaLibLearningExperimentRunner runner = new RaLibLearningExperimentRunner(logger);
-        runner.setSeed(7);
-        runner.setMaxDepth(4);
-        runner.run(RaLearningAlgorithmName.RATTT, dwOracle, teachers, consts, jsolv, new ParameterizedSymbol [] {OFFER, POLL});
+        runner.setMaxDepth(6);
+//        runner.setUseOldAnalyzer(true);
         
-//        for (int i=0; i<SEEDS; i++) {
-//            runner.setSeed(i);
-//            runner.setMaxDepth(4);
-//            runner.run(RaLearningAlgorithmName.RATTT, dwOracle, teachers, consts, jsolv, new ParameterizedSymbol [] {OFFER, POLL});
-//        }
+        Measurements[] ratttCount = new Measurements [SEEDS];
+        Measurements[] rastarCount = new Measurements [SEEDS];
+        for (int i=0; i<SEEDS; i++) {
+            System.out.println(i);
+            runner.setSeed(i);
+            runner.run(RaLearningAlgorithmName.RATTT, dwOracle, teachers, consts, jsolv, new ParameterizedSymbol [] {OFFER, POLL});
+            ratttCount[i] = runner.getMeasurements();
+            runner.resetMeasurements();
+            runner.run(RaLearningAlgorithmName.RASTAR, dwOracle, teachers, consts, jsolv, new ParameterizedSymbol [] {OFFER, POLL});
+            rastarCount[i] = runner.getMeasurements();
+            runner.resetMeasurements();
+        }
+        
+        System.out.println("Queries (TTT): " + Arrays.toString(ratttCount));
+        System.out.println("Queries (Star): " + Arrays.toString(rastarCount));
     }
 }
