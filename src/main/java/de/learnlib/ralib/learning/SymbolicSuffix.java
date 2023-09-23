6a7471a98d7f7ceeaff619f28e80c2a4a40e7e8e
Kostis Sagonas <kostis@it.uu.se>
2023-09-23 10:42:38 +0200

    Fix calls to deprecated APIs
    The `Class.newInstance()` calls are deprecated.
    Change them to use the `Class.getDeclaredConstructor().newInstance()`
    form and handle the additional exceptions that can occur.

--- src/main/java/de/learnlib/ralib/learning/SymbolicSuffix.java
+++ src/main/java/de/learnlib/ralib/learning/SymbolicSuffix.java
@@ -67,9 +67,9 @@ public class SymbolicSuffix {
     	actions = Word.fromWords(s.actions);
 
     	for (SuffixValue sv : s.freeValues)
-    		freeValues.add(sv.copy());
+            freeValues.add(sv.copy());
     	for (Map.Entry<Integer, SuffixValue> dv : s.dataValues.entrySet())
-    		dataValues.put(new Integer(dv.getKey()), dv.getValue().copy());
+            dataValues.put(Integer.valueOf(dv.getKey()), dv.getValue().copy());
     }
 
     /**
