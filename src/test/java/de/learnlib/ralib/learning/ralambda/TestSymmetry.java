bf529e7035505b1b8ebd6fbc5d79c5c2711c0df5
Fredrik Tåquist <fredriktaquist@gmail.com>
2023-09-28 11:00:21 +0200

    remove fix that fixes a mistake that is not, in fact, a mistake

--- src/test/java/de/learnlib/ralib/learning/ralambda/TestSymmetry.java
+++ src/test/java/de/learnlib/ralib/learning/ralambda/TestSymmetry.java
@@ -230,7 +230,7 @@ public class TestSymmetry extends RaLibTestSuite {
         		new PSymbolInstance(A, new DataValue(T_INT, 1)),
         		new PSymbolInstance(B, new DataValue(T_INT, 2)),
         		new PSymbolInstance(B, new DataValue(T_INT, 3)),
-        		new PSymbolInstance(B, new DataValue(T_INT, 1)));
+        		new PSymbolInstance(B, new DataValue(T_INT, 2)));
 
         Assert.assertTrue(hyp.accepts(ce));
 	}
