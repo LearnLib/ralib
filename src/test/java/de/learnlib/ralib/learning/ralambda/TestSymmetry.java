7215d3bba04823c69f8d909fb5a8dd8a33381679
Kostis Sagonas <kostis@it.uu.se>
2024-04-17 13:35:41 +0200

    Clean up tests to check expected results rather than print them (#80)
    Various tests in the test suite were just printing out the expected
    results rather than checking assertions with expected outcomes, which
    meant that regressions could escape attention. In addition, the test
    suite run was quite verbose.
    
    This PR clean up most of the tests by adding appropriate assertions
    and comments out calls to printing functions. While at it, enforced
    a more homogeneous naming of tests and style of writing the code.

--- src/test/java/de/learnlib/ralib/learning/ralambda/TestSymmetry.java
+++ src/test/java/de/learnlib/ralib/learning/ralambda/TestSymmetry.java
@@ -42,172 +42,167 @@ import net.automatalib.word.Word;
 
 public class TestSymmetry extends RaLibTestSuite {
 
-	private static final DataType T_INT = new DataType("int", Integer.class);
+    private static final DataType T_INT = new DataType("int", Integer.class);
 
-	private static final InputSymbol A =
-			new InputSymbol("a", new DataType[] {T_INT});
-	private static final InputSymbol B =
-			new InputSymbol("b", new DataType[] {T_INT});
+    private static final InputSymbol A = new InputSymbol("a", new DataType[] {T_INT});
+    private static final InputSymbol B = new InputSymbol("b", new DataType[] {T_INT});
 
-	@Test
-	public void learnSymmetryExampleCT2() {
+    @Test
+    public void learnSymmetryExampleCT2() {
+	Constants consts = new Constants();
+	RegisterAutomaton sul = buildCT2();
+	DataWordOracle dwOracle = new SimulatorOracle(sul);
 
-		Constants consts = new Constants();
-		RegisterAutomaton sul = buildCT2();
-		DataWordOracle dwOracle = new SimulatorOracle(sul);
+	final Map<DataType, Theory> teachers = new LinkedHashMap<>();
+	teachers.put(T_INT, new IntegerEqualityTheory(T_INT));
 
-		final Map<DataType, Theory> teachers = new LinkedHashMap<>();
-		teachers.put(T_INT, new IntegerEqualityTheory(T_INT));
+	ConstraintSolver solver = new SimpleConstraintSolver();
 
-		ConstraintSolver solver = new SimpleConstraintSolver();
+	MultiTheoryTreeOracle mto = new MultiTheoryTreeOracle(dwOracle, teachers, new Constants(), solver);
 
-		MultiTheoryTreeOracle mto = new MultiTheoryTreeOracle(dwOracle, teachers, new Constants(), solver);
+	SDTLogicOracle slo = new MultiTheorySDTLogicOracle(consts, solver);
 
-		SDTLogicOracle slo = new MultiTheorySDTLogicOracle(consts, solver);
+	TreeOracleFactory hypFactory = (RegisterAutomaton hyp) ->
+	    new MultiTheoryTreeOracle(new SimulatorOracle(hyp), teachers,
+				      new Constants(), solver);
 
-		TreeOracleFactory hypFactory = (RegisterAutomaton hyp) ->
-				new MultiTheoryTreeOracle(new SimulatorOracle(hyp), teachers,
-						new Constants(), solver);
+	// System.out.println(sul);
+	// System.out.println("---------------------------------");
 
-		System.out.println(sul);
-		System.out.println("---------------------------------");
+	RaLambda learner = new RaLambda(mto, hypFactory, slo, consts, false, false, A, B);
+	learner.setSolver(solver);
+	learner.learn();
 
-		RaLambda learner = new RaLambda(mto, hypFactory, slo, consts, false, false, A, B);
-		learner.setSolver(solver);
+	Word<PSymbolInstance> ce =
+	    Word.fromSymbols(new PSymbolInstance(A, new DataValue(T_INT, 1)),
+			     new PSymbolInstance(A, new DataValue(T_INT, 2)),
+			     new PSymbolInstance(A, new DataValue(T_INT, 1)));
+	learner.addCounterexample(new DefaultQuery<>(ce, true));
+	learner.learn();
 
-		learner.learn();
+	Hypothesis hyp = learner.getHypothesis();
+	// System.out.println(hyp.toString());
+	// System.out.println(learner.getDT());
 
-		Word<PSymbolInstance> ce = Word.fromSymbols(
-				new PSymbolInstance(A, new DataValue(T_INT, 1)),
-				new PSymbolInstance(A, new DataValue(T_INT, 2)),
-				new PSymbolInstance(A, new DataValue(T_INT, 1)));
+	ce = Word.fromSymbols(new PSymbolInstance(A, new DataValue(T_INT, 1)),
+			      new PSymbolInstance(A, new DataValue(T_INT, 2)),
+			      new PSymbolInstance(B, new DataValue(T_INT, 3)),
+			      new PSymbolInstance(B, new DataValue(T_INT, 4)),
+			      new PSymbolInstance(B, new DataValue(T_INT, 2)),
+			      new PSymbolInstance(B, new DataValue(T_INT, 1)));
 
-		learner.addCounterexample(new DefaultQuery<>(ce, true));
-		learner.learn();
+	Assert.assertTrue(sul.accepts(ce));
+	Assert.assertFalse(hyp.accepts(ce));
 
-		Hypothesis hyp = learner.getHypothesis();
-		System.out.println(learner.getHypothesis().toString());
-		System.out.println(learner.getDT());
+	learner.addCounterexample(new DefaultQuery<>(ce, true));
+	learner.learn();
 
-		ce = Word.fromSymbols(
-				new PSymbolInstance(A, new DataValue(T_INT, 1)),
-				new PSymbolInstance(A, new DataValue(T_INT, 2)),
-				new PSymbolInstance(B, new DataValue(T_INT, 3)),
-				new PSymbolInstance(B, new DataValue(T_INT, 4)),
-				new PSymbolInstance(B, new DataValue(T_INT, 2)),
-				new PSymbolInstance(B, new DataValue(T_INT, 1)));
+	// System.out.println(learner.getHypothesis().toString());
+	// System.out.println(learner.getDT());
+	Assert.assertTrue(learner.getDTHyp().accepts(ce));
+    }
 
-		System.out.println(sul.accepts(ce));
-		System.out.println(hyp.accepts(ce));
+    private RegisterAutomaton buildCT2() {
+	MutableRegisterAutomaton ra = new MutableRegisterAutomaton();
 
-		learner.addCounterexample(new DefaultQuery<>(ce, true));
-		learner.learn();
+	RALocation l_eps = ra.addInitialState(false);
+	RALocation l_a = ra.addState(false);
+	RALocation l_aa = ra.addState(false);
+	RALocation l_aab = ra.addState(false);
+	RALocation l_aabb = ra.addState(false);
+	RALocation l_aabbb = ra.addState(false);
 
-		System.out.println(learner.getHypothesis().toString());
-		System.out.println(learner.getDT());
-		Assert.assertTrue(learner.getDTHyp().accepts(ce));
-	}
+	RALocation l_sink = ra.addState(false);
+	RALocation l_final = ra.addState(true);
 
-	private RegisterAutomaton buildCT2() {
-		MutableRegisterAutomaton ra = new MutableRegisterAutomaton();
+	SymbolicDataValueGenerator.RegisterGenerator rgen = new SymbolicDataValueGenerator.RegisterGenerator();
+	SymbolicDataValue.Register r1 = rgen.next(T_INT);
+	SymbolicDataValue.Register r2 = rgen.next(T_INT);
+	SymbolicDataValueGenerator.ParameterGenerator pgen = new SymbolicDataValueGenerator.ParameterGenerator();
+	SymbolicDataValue.Parameter p1 = pgen.next(T_INT);
 
-		RALocation l_eps = ra.addInitialState(false);
-		RALocation l_a = ra.addState(false);
-		RALocation l_aa = ra.addState(false);
-		RALocation l_aab = ra.addState(false);
-		RALocation l_aabb = ra.addState(false);
-		RALocation l_aabbb = ra.addState(false);
+	TransitionGuard trueGuard = new TransitionGuard();
+	TransitionGuard equalR1 =
+	    new TransitionGuard(new AtomicGuardExpression(r1, Relation.EQUALS, p1));
+	TransitionGuard notEqualR1 =
+	    new TransitionGuard(new AtomicGuardExpression(r1, Relation.NOT_EQUALS, p1));
 
-		RALocation l_sink = ra.addState(false);
-		RALocation l_final = ra.addState(true);
+	TransitionGuard disjunctionGuard =
+	    new TransitionGuard(new Disjunction(new AtomicGuardExpression(r1, Relation.EQUALS, p1),
+						new AtomicGuardExpression(r2, Relation.EQUALS, p1)));
 
-		SymbolicDataValueGenerator.RegisterGenerator rgen = new SymbolicDataValueGenerator.RegisterGenerator();
-		SymbolicDataValue.Register r1 = rgen.next(T_INT);
-		SymbolicDataValue.Register r2 = rgen.next(T_INT);
-		SymbolicDataValueGenerator.ParameterGenerator pgen = new SymbolicDataValueGenerator.ParameterGenerator();
-		SymbolicDataValue.Parameter p1 = pgen.next(T_INT);
-
-		TransitionGuard trueGuard = new TransitionGuard();
-		TransitionGuard equalR1 = new TransitionGuard(new AtomicGuardExpression(r1, Relation.EQUALS, p1));
-		TransitionGuard notEqualR1 = new TransitionGuard(new AtomicGuardExpression(r1, Relation.NOT_EQUALS, p1));
-
-		TransitionGuard disjunctionGuard = new TransitionGuard(new Disjunction(
-				new AtomicGuardExpression(r1, Relation.EQUALS, p1),
-				new AtomicGuardExpression(r2, Relation.EQUALS, p1)));
-
-		TransitionGuard elseGuard = new TransitionGuard(new Conjunction(
-				new AtomicGuardExpression(r1, Relation.NOT_EQUALS, p1),
-				new AtomicGuardExpression(r2, Relation.NOT_EQUALS, p1)));
+	TransitionGuard elseGuard =
+	    new TransitionGuard(new Conjunction(new AtomicGuardExpression(r1, Relation.NOT_EQUALS, p1),
+						new AtomicGuardExpression(r2, Relation.NOT_EQUALS, p1)));
 
 
-		VarMapping<SymbolicDataValue.Register, SymbolicDataValue> storeR1Mapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
-		storeR1Mapping.put(r1, p1);
-		VarMapping<SymbolicDataValue.Register, SymbolicDataValue> storeR1R2Mapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
-		storeR1R2Mapping.put(r1, p1);
-		storeR1R2Mapping.put(r2, r1);
-		VarMapping<SymbolicDataValue.Register, SymbolicDataValue> copyMapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
-		copyMapping.put(r1, r1);
-		copyMapping.put(r2, r2);
+	VarMapping<SymbolicDataValue.Register, SymbolicDataValue> storeR1Mapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
+	storeR1Mapping.put(r1, p1);
+	VarMapping<SymbolicDataValue.Register, SymbolicDataValue> storeR1R2Mapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
+	storeR1R2Mapping.put(r1, p1);
+	storeR1R2Mapping.put(r2, r1);
+	VarMapping<SymbolicDataValue.Register, SymbolicDataValue> copyMapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
+	copyMapping.put(r1, r1);
+	copyMapping.put(r2, r2);
 
-		VarMapping<SymbolicDataValue.Register, SymbolicDataValue> forgetMapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
-		forgetMapping.put(r1, r2);
+	VarMapping<SymbolicDataValue.Register, SymbolicDataValue> forgetMapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
+	forgetMapping.put(r1, r2);
 
-		VarMapping<Register, SymbolicDataValue> noMapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
+	VarMapping<Register, SymbolicDataValue> noMapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
 
-		Assignment storeR1 = new Assignment(storeR1Mapping);
-		Assignment storeR1R2 = new Assignment(storeR1R2Mapping);
-		Assignment copy = new Assignment(copyMapping);
-		Assignment forget = new Assignment(forgetMapping);
-		Assignment noAssign = new Assignment(noMapping);
+	Assignment storeR1 = new Assignment(storeR1Mapping);
+	Assignment storeR1R2 = new Assignment(storeR1R2Mapping);
+	Assignment copy = new Assignment(copyMapping);
+	Assignment forget = new Assignment(forgetMapping);
+	Assignment noAssign = new Assignment(noMapping);
 
-		ra.addTransition(l_eps, A, new InputTransition(trueGuard, A, l_eps, l_a, storeR1));
-		ra.addTransition(l_eps, B, new InputTransition(trueGuard, B, l_eps, l_sink, noAssign));
+	ra.addTransition(l_eps, A, new InputTransition(trueGuard, A, l_eps, l_a, storeR1));
+	ra.addTransition(l_eps, B, new InputTransition(trueGuard, B, l_eps, l_sink, noAssign));
 
-		ra.addTransition(l_a, A, new InputTransition(trueGuard, A, l_a, l_aa, storeR1R2));
-		ra.addTransition(l_a, B, new InputTransition(trueGuard, B, l_eps, l_sink, noAssign));
+	ra.addTransition(l_a, A, new InputTransition(trueGuard, A, l_a, l_aa, storeR1R2));
+	ra.addTransition(l_a, B, new InputTransition(trueGuard, B, l_eps, l_sink, noAssign));
 
-		ra.addTransition(l_aa, A, new InputTransition(elseGuard, A, l_aa, l_sink, noAssign));
-		ra.addTransition(l_aa, A, new InputTransition(disjunctionGuard, A, l_aa, l_final, noAssign));
-		ra.addTransition(l_aa, B, new InputTransition(trueGuard, B, l_aa, l_aab, copy));
+	ra.addTransition(l_aa, A, new InputTransition(elseGuard, A, l_aa, l_sink, noAssign));
+	ra.addTransition(l_aa, A, new InputTransition(disjunctionGuard, A, l_aa, l_final, noAssign));
+	ra.addTransition(l_aa, B, new InputTransition(trueGuard, B, l_aa, l_aab, copy));
 
-		ra.addTransition(l_aab, A, new InputTransition(trueGuard, A, l_aab, l_sink, noAssign));
-		ra.addTransition(l_aab, B, new InputTransition(trueGuard, B, l_aab, l_aabb, copy));
+	ra.addTransition(l_aab, A, new InputTransition(trueGuard, A, l_aab, l_sink, noAssign));
+	ra.addTransition(l_aab, B, new InputTransition(trueGuard, B, l_aab, l_aabb, copy));
 
-		ra.addTransition(l_aabb, A, new InputTransition(trueGuard, A, l_aabb, l_sink, noAssign));
-		ra.addTransition(l_aabb, B, new InputTransition(notEqualR1, B, l_aabb, l_sink, noAssign));
-		ra.addTransition(l_aabb, B, new InputTransition(equalR1, B, l_aabb, l_aabbb, forget));
+	ra.addTransition(l_aabb, A, new InputTransition(trueGuard, A, l_aabb, l_sink, noAssign));
+	ra.addTransition(l_aabb, B, new InputTransition(notEqualR1, B, l_aabb, l_sink, noAssign));
+	ra.addTransition(l_aabb, B, new InputTransition(equalR1, B, l_aabb, l_aabbb, forget));
 
-		ra.addTransition(l_aabbb, A, new InputTransition(trueGuard, A, l_aabbb, l_sink, noAssign));
-		ra.addTransition(l_aabbb, B, new InputTransition(notEqualR1, B, l_aabbb, l_sink, noAssign));
-		ra.addTransition(l_aabbb, B, new InputTransition(equalR1, B, l_aabbb, l_final, noAssign));
+	ra.addTransition(l_aabbb, A, new InputTransition(trueGuard, A, l_aabbb, l_sink, noAssign));
+	ra.addTransition(l_aabbb, B, new InputTransition(notEqualR1, B, l_aabbb, l_sink, noAssign));
+	ra.addTransition(l_aabbb, B, new InputTransition(equalR1, B, l_aabbb, l_final, noAssign));
 
-		ra.addTransition(l_final, A, new InputTransition(trueGuard, A, l_final, l_final, noAssign));
-		ra.addTransition(l_final, B, new InputTransition(trueGuard, B, l_final, l_final, noAssign));
-		ra.addTransition(l_sink, A, new InputTransition(trueGuard, A, l_sink, l_sink, noAssign));
-		ra.addTransition(l_sink, B, new InputTransition(trueGuard, B, l_sink, l_sink, noAssign));
+	ra.addTransition(l_final, A, new InputTransition(trueGuard, A, l_final, l_final, noAssign));
+	ra.addTransition(l_final, B, new InputTransition(trueGuard, B, l_final, l_final, noAssign));
+	ra.addTransition(l_sink, A, new InputTransition(trueGuard, A, l_sink, l_sink, noAssign));
+	ra.addTransition(l_sink, B, new InputTransition(trueGuard, B, l_sink, l_sink, noAssign));
 
-		return ra;
-	}
+	return ra;
+    }
 
-	@Test
-	public void learnSymmetryExampleCT() {
+    @Test
+    public void learnSymmetryExampleCT() {
+	Constants consts = new Constants();
+	RegisterAutomaton sul = buildCT();
+	DataWordOracle dwOracle = new SimulatorOracle(sul);
 
-		Constants consts = new Constants();
-		RegisterAutomaton sul = buildCT();
-	    DataWordOracle dwOracle = new SimulatorOracle(sul);
+	final Map<DataType, Theory> teachers = new LinkedHashMap<>();
+	teachers.put(T_INT, new IntegerEqualityTheory(T_INT));
 
-	    final Map<DataType, Theory> teachers = new LinkedHashMap<>();
-	    teachers.put(T_INT, new IntegerEqualityTheory(T_INT));
+	ConstraintSolver solver = new SimpleConstraintSolver();
 
-	    ConstraintSolver solver = new SimpleConstraintSolver();
-
-	    MultiTheoryTreeOracle mto = new MultiTheoryTreeOracle(dwOracle, teachers, new Constants(), solver);
+	MultiTheoryTreeOracle mto = new MultiTheoryTreeOracle(dwOracle, teachers, new Constants(), solver);
 
         SDTLogicOracle slo = new MultiTheorySDTLogicOracle(consts, solver);
 
         TreeOracleFactory hypFactory = (RegisterAutomaton hyp) ->
-                new MultiTheoryTreeOracle(new SimulatorOracle(hyp), teachers,
-                        new Constants(), solver);
+	    new MultiTheoryTreeOracle(new SimulatorOracle(hyp), teachers,
+				      new Constants(), solver);
 
         RaLambda learner = new RaLambda(mto, hypFactory, slo, consts, false, false, A, B);
         learner.setSolver(solver);
@@ -224,7 +219,7 @@ public class TestSymmetry extends RaLibTestSuite {
         learner.learn();
 
         Hypothesis hyp = learner.getHypothesis();
-        System.out.println(learner.getHypothesis().toString());
+        // System.out.println(learner.getHypothesis().toString());
 
         ce = Word.fromSymbols(
         		new PSymbolInstance(A, new DataValue(T_INT, 1)),
@@ -233,38 +228,38 @@ public class TestSymmetry extends RaLibTestSuite {
         		new PSymbolInstance(B, new DataValue(T_INT, 2)));
 
         Assert.assertTrue(hyp.accepts(ce));
-	}
+    }
 
-	private RegisterAutomaton buildCT() {
-		MutableRegisterAutomaton ra = new MutableRegisterAutomaton();
+    private RegisterAutomaton buildCT() {
+	MutableRegisterAutomaton ra = new MutableRegisterAutomaton();
 
-		RALocation l0 = ra.addInitialState(false);
-		RALocation l1 = ra.addState(false);
-		RALocation l2 = ra.addState(false);
-		RALocation l3 = ra.addState(true);
-		RALocation l4 = ra.addState(false);
+	RALocation l0 = ra.addInitialState(false);
+	RALocation l1 = ra.addState(false);
+	RALocation l2 = ra.addState(false);
+	RALocation l3 = ra.addState(true);
+	RALocation l4 = ra.addState(false);
 
-		SymbolicDataValueGenerator.RegisterGenerator rgen = new SymbolicDataValueGenerator.RegisterGenerator();
-		SymbolicDataValue.Register r1 = rgen.next(T_INT);
-		SymbolicDataValue.Register r2 = rgen.next(T_INT);
-		SymbolicDataValueGenerator.ParameterGenerator pgen = new SymbolicDataValueGenerator.ParameterGenerator();
-		SymbolicDataValue.Parameter p1 = pgen.next(T_INT);
+	SymbolicDataValueGenerator.RegisterGenerator rgen = new SymbolicDataValueGenerator.RegisterGenerator();
+	SymbolicDataValue.Register r1 = rgen.next(T_INT);
+	SymbolicDataValue.Register r2 = rgen.next(T_INT);
+	SymbolicDataValueGenerator.ParameterGenerator pgen = new SymbolicDataValueGenerator.ParameterGenerator();
+	SymbolicDataValue.Parameter p1 = pgen.next(T_INT);
 
-		TransitionGuard trueGuard = new TransitionGuard();
+	TransitionGuard trueGuard = new TransitionGuard();
         TransitionGuard equalR1 = new TransitionGuard(new AtomicGuardExpression(r1, Relation.EQUALS, p1));
         TransitionGuard notEqualR1 = new TransitionGuard(new AtomicGuardExpression(r1, Relation.NOT_EQUALS, p1));
         TransitionGuard disjunctionGuard = new TransitionGuard(new Disjunction(
         				new AtomicGuardExpression(r1, Relation.EQUALS, p1),
         				new AtomicGuardExpression(r2, Relation.EQUALS, p1)));
 
-		VarMapping<SymbolicDataValue.Register, SymbolicDataValue> storeR1Mapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
+	VarMapping<SymbolicDataValue.Register, SymbolicDataValue> storeR1Mapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
         storeR1Mapping.put(r1, p1);
-		VarMapping<SymbolicDataValue.Register, SymbolicDataValue> storeR2Mapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
+	VarMapping<SymbolicDataValue.Register, SymbolicDataValue> storeR2Mapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
         storeR2Mapping.put(r2, p1);
-		VarMapping<SymbolicDataValue.Register, SymbolicDataValue> storeR1R2Mapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
-		storeR1R2Mapping.put(r2, r1);
+	VarMapping<SymbolicDataValue.Register, SymbolicDataValue> storeR1R2Mapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
+	storeR1R2Mapping.put(r2, r1);
         storeR1R2Mapping.put(r1, p1);
-		VarMapping<SymbolicDataValue.Register, SymbolicDataValue> copyMapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
+	VarMapping<SymbolicDataValue.Register, SymbolicDataValue> copyMapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
         copyMapping.put(r1, r2);
         VarMapping<Register, SymbolicDataValue> noMapping = new VarMapping<SymbolicDataValue.Register, SymbolicDataValue>();
 
@@ -287,5 +282,5 @@ public class TestSymmetry extends RaLibTestSuite {
         ra.addTransition(l4, B, new InputTransition(trueGuard, B, l4, l4, noAssign));
 
         return ra;
-	}
+    }
 }
