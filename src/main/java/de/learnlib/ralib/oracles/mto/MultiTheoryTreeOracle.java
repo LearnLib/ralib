fc2bbdb22dacba82247e5f9f156bd266d2d143ec
Fredrik Tåquist <fredriktaquist@gmail.com>
2025-01-31 16:59:51 +0100

    fix to keep codespell happy

--- src/main/java/de/learnlib/ralib/oracles/mto/MultiTheoryTreeOracle.java
+++ src/main/java/de/learnlib/ralib/oracles/mto/MultiTheoryTreeOracle.java
@@ -164,7 +164,7 @@ public class MultiTheoryTreeOracle implements TreeOracle, SDTConstructor {
     }
 
     /**
-     * This method computes the initial branching for an SDT. It re-uses existing
+     * This method computes the initial branching for an SDT. It reuses existing
      * valuations where possible.
      *
      */
@@ -498,7 +498,7 @@ public class MultiTheoryTreeOracle implements TreeOracle, SDTConstructor {
     }
 
     /**
-     * This method computes the initial branching for an SDT. It re-uses existing
+     * This method computes the initial branching for an SDT. It reuses existing
      * valuations where possible.
      *
      */
