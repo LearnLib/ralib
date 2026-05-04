e3fe9d6f4f97b926363438bce5bdb27dfc353531
Fredrik Tåquist <fredriktaquist@gmail.com>
2026-05-04 12:50:41 +0200

    add palindrome sut

---
+++ src/test/java/de/learnlib/ralib/example/palindrome/PalindromeOracle.java
@@ -0,0 +1,46 @@
+package de.learnlib.ralib.example.palindrome;
+
+import java.util.Collection;
+
+import de.learnlib.query.Query;
+import de.learnlib.ralib.data.DataType;
+import de.learnlib.ralib.oracles.DataWordOracle;
+import de.learnlib.ralib.words.InputSymbol;
+import de.learnlib.ralib.words.PSymbolInstance;
+import net.automatalib.word.Word;
+
+public class PalindromeOracle implements DataWordOracle {
+
+	public static final DataType TYPE = new DataType("int");
+	public static final InputSymbol in = new InputSymbol("in", new DataType[] {TYPE});
+
+	private final Palindrome pal;
+
+	public PalindromeOracle(Palindrome pal) {
+		this.pal = pal;
+	}
+
+	@Override
+	public void processQueries(Collection<? extends Query<PSymbolInstance, Boolean>> queries) {
+		for (Query<PSymbolInstance, Boolean> q : queries) {
+			q.answer(answer(q.getInput()));
+		}
+	}
+
+	private boolean answer(Word<PSymbolInstance> word) {
+		pal.reset();
+		boolean isPalindrome = true;
+		for (PSymbolInstance psi : word) {
+			isPalindrome = answer(psi);
+		}
+		return isPalindrome;
+	}
+
+	private boolean answer(PSymbolInstance psi) {
+		if (!psi.getBaseSymbol().equals(in)) {
+			return false;
+		}
+		int d = psi.getParameterValues()[0].getValue().intValue();
+		return pal.in(d);
+	}
+}
