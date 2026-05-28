ce43dd95d6f386bdc20f96003a96ab96e03c60a7
Fredrik Tåquist <fredriktaquist@gmail.com>
2026-05-28 14:05:34 +0200

    Fix stats

--- src/test/java/de/learnlib/ralib/learning/ralambda/TestSuffixOptimization.java
+++ src/test/java/de/learnlib/ralib/learning/ralambda/TestSuffixOptimization.java
@@ -164,9 +164,9 @@ public class TestSuffixOptimization extends RaLibTestSuite {
         Assert.assertTrue(str.contains("Counterexamples: 1"));
         Assert.assertTrue(str.contains("CE max length: 4"));
         Assert.assertTrue(str.contains("CE Analysis: {TQ: 81, Resets: 92, Inputs: 0}"));
-        Assert.assertTrue(str.contains("Processing / Refinement: {TQ: 27, Resets: 548, Inputs: 0}"));
+        Assert.assertTrue(str.contains("Processing / Refinement: {TQ: 27, Resets: 514, Inputs: 0}"));
         Assert.assertTrue(str.contains("Other: {TQ: 5, Resets: 5, Inputs: 0}"));
-        Assert.assertTrue(str.contains("Total: {TQ: 113, Resets: 645, Inputs: 0}"));
+        Assert.assertTrue(str.contains("Total: {TQ: 113, Resets: 611, Inputs: 0}"));
     }
 
     @Test
