3e1247ef1292e6d13e3ed79c868bc5c94606adab
Fredrik Tåquist <fredriktaquist@gmail.com>
2026-09-03 16:04:44 +0200

    Add documentation to new interfaces

--- src/main/java/de/learnlib/ralib/theory/RestrictionContainer.java
+++ src/main/java/de/learnlib/ralib/theory/RestrictionContainer.java
@@ -2,11 +2,28 @@ package de.learnlib.ralib.theory;
 
 public interface RestrictionContainer {
 
+	/**
+	 * @param restr
+	 * @return {@code true} if and only if {@code this} contains {@code restr}
+	 */
 	public boolean contains(AbstractSuffixValueRestriction restr);
 
+	/**
+	 * @return {@code true} if and only if {@code this} contains an {@code UnmappedEqualityRestriction}
+	 */
 	public boolean containsUnmapped();
 
-	public AbstractSuffixValueRestriction replace(AbstractSuffixValueRestriction replace, AbstractSuffixValueRestriction with);
+	/**
+	 * @param replace
+	 * @param by
+	 * @return {@code this} with all instances of {@code replace} replaced by {@code by}
+	 */
+	public AbstractSuffixValueRestriction replace(AbstractSuffixValueRestriction replace, AbstractSuffixValueRestriction by);
 
+	/**
+	 * Case {@code this} to an {@code AbstractSuffixValueRestriction}
+	 *
+	 * @return
+	 */
 	public AbstractSuffixValueRestriction cast();
 }
