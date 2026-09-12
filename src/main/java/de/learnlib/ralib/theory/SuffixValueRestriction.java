4341401d036b076658e84adf71cf26b4f9136803
Kostis Sagonas <kostis@it.uu.se>
2026-09-12 12:03:42 +0200

    Fix a use of hashCode()

--- src/main/java/de/learnlib/ralib/theory/SuffixValueRestriction.java
+++ src/main/java/de/learnlib/ralib/theory/SuffixValueRestriction.java
@@ -81,7 +81,7 @@ public class SuffixValueRestriction extends AbstractSuffixValueRestriction {
 	    @Override
 	    public int hashCode() {
 	        int hash = 7;
-	        hash = 97 * hash + Objects.hashCode(this.id);
+	        hash = 97 * hash + Integer.hashCode(this.id);
 	        hash = 97 * hash + Objects.hashCode(this.type);
 	        hash = 97 * hash + Objects.hashCode(this.getClass());
 	        return hash;
