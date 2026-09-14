5833a837d6da0ad71061dcf4f9d6e457ce11a13f
Fredrik Tåquist <fredriktaquist@gmail.com>
2026-09-14 11:45:45 +0200

    Remove unnecessary check

--- src/main/java/de/learnlib/ralib/ct/CTInnerNode.java
+++ src/main/java/de/learnlib/ralib/ct/CTInnerNode.java
@@ -117,11 +117,8 @@ public class CTInnerNode extends CTNode {
 			leaves.put(u, l);
 		}
 
-		// make sure all short prefixes of leaf are still short
 		for (ShortPrefix u : shorts) {
-			if (!(u instanceof ShortPrefix)) {
-				leaves.get(u).elevatePrefix(u, oracle, inputs);
-			}
+			assert u != null;
 		}
 
 		return leaves;
